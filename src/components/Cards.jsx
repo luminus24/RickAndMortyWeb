d6574f46926cf891d268f3e1c1b628db3c1e8772
luminus24 <luminus128@gmail.com>
2023-03-02 15:18:01 -0500

    Styles responsive finished

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,32 +2,34 @@ import React from 'react'
 import useGetCharacters from '../Hooks/useGetCharacters'
 import "../styles/cardStyles.css"
 
-const Cards = ({characterName}) => {
+const Cards = ({ characterName }) => {
 
    const characters = useGetCharacters(characterName)
 
    return (
       <div className='card'>
          {
-            characters?.results.map((character) =>(
+            characters?.results.map((character) => (
                <div key={character?.id} className="card-container">
-                  <img src={character?.image} alt="character image" className='img-character'/>
+                  <img src={character?.image} alt="character image" className='img-character' />
                   <h1 className='title'>{character?.name}</h1>
                   <div className='status-container'>
                      <div className={`${character?.status} status`}></div>
                      <h4 >{character?.status}</h4>
                   </div>
-                  <div className='genero'>
-                     <h4>Gènero: </h4>
-                     <span> {character?.gender}</span>
-                  </div>
-                  <div className='localizacion'>
-                     <h4>Localización: </h4>
-                     <span>{character?.location.name}</span>
-                  </div>
-                  <div className='episodios'>
-                     <h4>Episodios: </h4>
-                     <span>{character?.episode.length}</span>
+                  <div>
+                     <div className='genero'>
+                        <h4>Gènero: </h4>
+                        <span> {character?.gender}</span>
+                     </div>
+                     <div className='localizacion'>
+                        <h4>Localización: </h4>
+                        <span>{character?.location.name}</span>
+                     </div>
+                     <div className='episodios'>
+                        <h4>Episodios: </h4>
+                        <span>{character?.episode.length}</span>
+                     </div>
                   </div>
                </div>
             ))
