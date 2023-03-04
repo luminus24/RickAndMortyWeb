0e070e630be7621def82f6948b5371a5afd57c56
luminus24 <luminus128@gmail.com>
2023-03-04 16:58:55 -0500

    Pagination styles finished

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,55 +1,58 @@
 import React, { useState } from 'react'
 import useGetCharacters from '../Hooks/useGetCharacters'
 import "../styles/cardStyles.css"
+import "../styles/paginationStyles.css"
 
 const Cards = ({ characterName }) => {
    const [count, setCount] = useState(1)
-   const {characters, nextCharacters, prevCharacters, setActualCharacters} = useGetCharacters(characterName)
-   const OnclickNext = () =>{
+   const { characters, nextCharacters, prevCharacters, setActualCharacters } = useGetCharacters(characterName)
+   const OnclickNext = () => {
       setCount(count + 1)
       setActualCharacters(nextCharacters)
    }
 
-   const OnclickPrev = () =>{
-      if(count>0){
+   const OnclickPrev = () => {
+      if (count > 0) {
          setCount(count - 1)
       }
       setActualCharacters(prevCharacters)
    }
    return (
-      <div className='card'>
-         {
-            characters?.results.map((character) => (
-               <div key={character?.id} className="card-container">
-                  <img src={character?.image} alt="character image" className='img-character' />
-                  <h1 className='title'>{character?.name}</h1>
-                  <div className='status-container'>
-                     <div className={`${character?.status} status`}></div>
-                     <h4 >{character?.status}</h4>
+      <>
+         <div className='card'>
+            {
+               characters?.results.map((character) => (
+                  <div key={character?.id} className="card-container">
+                     <img src={character?.image} alt="character image" className='img-character' />
+                     <h1 className='title'>{character?.name}</h1>
+                     <div className='status-container'>
+                        <div className={`${character?.status} status`}></div>
+                        <h4 >{character?.status}</h4>
+                     </div>
+                     <div>
+                        <div className='genero'>
+                           <h4>Gènero: </h4>
+                           <span> {character?.gender}</span>
+                        </div>
+                        <div className='localizacion'>
+                           <h4>Localización: </h4>
+                           <span>{character?.location.name}</span>
+                        </div>
+                        <div className='episodios'>
+                           <h4>Episodios: </h4>
+                           <span>{character?.episode.length}</span>
+                        </div>
+                     </div>
                   </div>
-                  <div>
-                     <div className='genero'>
-                        <h4>Gènero: </h4>
-                        <span> {character?.gender}</span>
-                     </div>
-                     <div className='localizacion'>
-                        <h4>Localización: </h4>
-                        <span>{character?.location.name}</span>
-                     </div>
-                     <div className='episodios'>
-                        <h4>Episodios: </h4>
-                        <span>{character?.episode.length}</span>
-                     </div>
-                  </div>
-               </div>
-            ))
-         }
-         <div>
+               ))
+            }
+         </div>
+         <div className='pagination'>
             <button onClick={OnclickPrev}>Prev</button>
-            <div>{count}</div>
+            <div className='page'>{count}</div>
             <button onClick={OnclickNext}>Next</button>
          </div>
-      </div>
+      </>
    )
 }
 export default Cards
